9775dfb900e07ee08aaf0c70c6ba7b430e505d88
MatteoRossi01 <93929073+MatteoRossi01@users.noreply.github.com>
2022-03-03 22:31:24 +0100

    Inizializzazione vue cli

---
+++ keynote/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue';
+import Header from './components/Main.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
